b7e8cbfe432b3f2ec6c98d33dd69f7a808bf98c5
aluoyu <81433703+aluoyu@users.noreply.github.com>
2021-04-11 09:51:14 +0800

    Add files via upload

---
+++ Week3/LQueue/Headers/LQueue.h
@@ -0,0 +1,38 @@
+#ifndef LQUEUE_H_INCLUDED
+#define LQUEUE_H_INCLUDED
+
+int count;
+int outNum;
+typedef struct node
+{
+    void *data;                   //数据域指针
+    struct node *next;            //指向当前结点的下一结点
+} Node;
+
+typedef struct Lqueue
+{
+    Node *front;                   //队头
+    Node *rear;                    //队尾
+    size_t length;            //队列长度
+} LQueue;
+
+typedef enum
+{
+    FALSE=0, TRUE=1
+} Status;
+char type;
+char datatype[30];
+
+void InitLQueue(LQueue *Q);
+void DestoryLQueue(LQueue *Q);
+Status IsEmptyLQueue(const LQueue *Q);
+Status GetHeadLQueue(LQueue *Q, void *e);
+int LengthLQueue(LQueue *Q);
+Status EnLQueue(LQueue *Q, void *data);
+Status DeLQueue(LQueue *Q);
+void ClearLQueue(LQueue *Q);
+Status TraverseLQueue(const LQueue *Q, void (*foo)(void *q));
+void LPrint(void *q);
+
+
+#endif // LQUEUE_H_INCLUDED
